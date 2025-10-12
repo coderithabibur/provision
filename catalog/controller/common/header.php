8d1387cbd74b3017589481050d7c03a4791ea27d
Ashikur Rahman <ashiktpi30@gmail.com>
2025-10-12 13:09:23 +0600

    Remove $search from header controller

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -166,8 +166,7 @@ class ControllerCommonHeader extends Controller {
 		}
 
 		$data['language'] = $this->load->controller('common/language');
-		$data['currency'] = $this->load->controller('common/currency');
-		$data['search'] = $this->load->controller('common/search');
+		$data['currency'] = $this->load->controller('common/currency'); 
 		$data['cart'] = $this->load->controller('common/cart');
 
 		// Mini cart data 
