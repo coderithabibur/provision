1f19c70f17e8bdb090bc94e57222ecd45e4f9b24
Ashikur Rahman <ashiktpi30@gmail.com>
2025-10-13 06:35:34 +0600

    Fix Client Logos Module Admin Option

--- admin/controller/extension/module/client_logos.php
+++ admin/controller/extension/module/client_logos.php
@@ -1,109 +1,103 @@
 <?php
-class ControllerModuleClientLogos extends Controller {
+class ControllerExtensionModuleClientLogos extends Controller {
     private $error = array();
 
     public function index() {
-        // Load language and document title
-        $this->load->language('module/client_logos');
+        $this->load->language('extension/module/client_logos');
         $this->document->setTitle($this->language->get('heading_title'));
-
-        // Load the Setting Model
         $this->load->model('setting/setting');
 
-        // Handle POST Request (Saving the form data)
         if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-            // OC 2.x uses the module name as the config key
+            // For OC2, we use editSetting with the module's filename as the key
             $this->model_setting_setting->editSetting('client_logos', $this->request->post);
-
+            
             $this->session->data['success'] = $this->language->get('text_success');
-
-            // Redirect back to the module list
+            
+            // Correct redirect for OC2.1 using 'extension/module' and 'token'
             $this->response->redirect($this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'));
         }
 
-        // --- Prepare Data for View ---
         $data['heading_title'] = $this->language->get('heading_title');
-        
-        // Language variables
         $data['text_edit'] = $this->language->get('text_edit');
         $data['text_enabled'] = $this->language->get('text_enabled');
         $data['text_disabled'] = $this->language->get('text_disabled');
-        $data['entry_status'] = $this->language->get('entry_status');
+        $data['entry_title'] = $this->language->get('entry_title');
         $data['entry_image'] = $this->language->get('entry_image');
         $data['entry_link'] = $this->language->get('entry_link');
-        $data['entry_alt'] = $this->language->get('entry_alt');
+        $data['entry_sort_order'] = $this->language->get('entry_sort_order');
+        $data['entry_status'] = $this->language->get('entry_status');
         $data['button_save'] = $this->language->get('button_save');
         $data['button_cancel'] = $this->language->get('button_cancel');
-        $data['button_add'] = $this->language->get('button_add');
+        $data['button_logo_add'] = $this->language->get('button_logo_add');
         $data['button_remove'] = $this->language->get('button_remove');
         
-        // Check for Errors
         if (isset($this->error['warning'])) {
             $data['error_warning'] = $this->error['warning'];
         } else {
             $data['error_warning'] = '';
         }
 
-        // Breadcrumbs
         $data['breadcrumbs'] = array();
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_home'),
-            'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
-        );
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_module'),
-            'href' => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL')
-        );
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('module/client_logos', 'token=' . $this->session->data['token'], 'SSL')
-        );
+        $data['breadcrumbs'][] = array('text' => $this->language->get('text_home'), 'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL'));
+        $data['breadcrumbs'][] = array('text' => $this->language->get('text_extension'),'href' => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'));
+        $data['breadcrumbs'][] = array('text' => $this->language->get('heading_title'), 'href' => $this->url->link('extension/module/client_logos', 'token=' . $this->session->data['token'], 'SSL'));
 
-        // Action URLs
-        $data['action'] = $this->url->link('module/client_logos', 'token=' . $this->session->data['token'], 'SSL');
+        $data['action'] = $this->url->link('extension/module/client_logos', 'token=' . $this->session->data['token'], 'SSL');
         $data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
+        
+        if (isset($this->request->post['client_logos_title'])) {
+            $data['client_logos_title'] = $this->request->post['client_logos_title'];
+        } else {
+            $data['client_logos_title'] = $this->config->get('client_logos_title');
+        }
 
-        // Fetch Status Setting
+        $this->load->model('tool/image');
+        $data['placeholder'] = $this->model_tool_image->resize('no_image.png', 100, 100);
+        
+        $data['client_logos'] = array();
+        if (isset($this->request->post['client_logos_logo'])) {
+            $logos = $this->request->post['client_logos_logo'];
+        } else {
+            $logos = $this->config->get('client_logos_logo');
+        }
+
+        if (!empty($logos)) {
+            foreach ($logos as $logo) {
+                if (is_file(DIR_IMAGE . $logo['image'])) {
+                    $image = $logo['image'];
+                    $thumb = $logo['image'];
+                } else {
+                    $image = '';
+                    $thumb = 'no_image.png';
+                }
+                $data['client_logos'][] = array(
+                    'image'      => $image,
+                    'thumb'      => $this->model_tool_image->resize($thumb, 100, 100),
+                    'link'       => $logo['link'],
+                    'sort_order' => $logo['sort_order']
+                );
+            }
+        }
+        
         if (isset($this->request->post['client_logos_status'])) {
             $data['client_logos_status'] = $this->request->post['client_logos_status'];
         } else {
             $data['client_logos_status'] = $this->config->get('client_logos_status');
         }
-        
-        // --- Fetch Dynamic Logo Items ---
-        if (isset($this->request->post['client_logos_item'])) {
-            $data['client_logos_items'] = $this->request->post['client_logos_item'];
-        } elseif ($this->config->get('client_logos_item')) {
-            $data['client_logos_items'] = $this->config->get('client_logos_item');
-        } else {
-            $data['client_logos_items'] = array();
-        }
 
-        // Load Image Model for Image Management (Thumbnails)
-        $this->load->model('tool/image');
-        $data['placeholder'] = $this->model_tool_image->resize('no_image.png', 100, 100);
-
-        // Add a thumbnail for each logo item
-        foreach ($data['client_logos_items'] as &$item) {
-            $item['thumb'] = $this->model_tool_image->resize($item['image'], 100, 100);
-        }
-        unset($item);
-
-        // Load common elements
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['footer'] = $this->load->controller('common/footer');
 
-        $this->response->setOutput($this->load->view('module/client_logos.tpl', $data));
+        $this->response->setOutput($this->load->view('extension/module/client_logos.twig', $data));
     }
 
     protected function validate() {
-        if (!$this->user->hasPermission('modify', 'module/client_logos')) {
+        if (!$this->user->hasPermission('modify', 'extension/module/client_logos')) {
             $this->error['warning'] = $this->language->get('error_permission');
         }
 
-        // Optional: Add validation for required fields
-        
         return !$this->error;
     }
 }
+
--- admin/language/en-gb/extension/module/client_logos.php
+++ admin/language/en-gb/extension/module/client_logos.php
@@ -1,17 +1,23 @@
 <?php
 // Heading
-$_['heading_title']    = 'Client Logos Slider';
+$_['heading_title']    = 'Client Logos';
 
 // Text
-$_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified Client Logos module!';
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified the Client Logos module!';
 $_['text_edit']        = 'Edit Client Logos Module';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_name']       = 'Module Name';
+$_['entry_title']      = 'Section Title';
 $_['entry_image']      = 'Logo Image';
-$_['entry_link']       = 'Link URL (Optional)';
-$_['entry_alt']        = 'Alt Text';
+$_['entry_link']       = 'Link';
+$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status']     = 'Status';
+
+// Button
+$_['button_logo_add']  = 'Add Logo';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify Client Logos module!';
+$_['error_permission'] = 'Warning: You do not have permission to modify the Client Logos module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
