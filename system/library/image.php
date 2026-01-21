7e68a7553d650685dcc2152439f98b937ceb11c0
Ashikur Rahman <ashiktpi30@gmail.com>
2026-01-21 21:48:30 +0600

    Revert "speed update 10"
    This reverts commit 280eec413c3e8b1fcb0e543eb8df155407d9fabe.

--- system/library/image.php
+++ system/library/image.php
@@ -69,7 +69,7 @@ class Image {
 			} elseif ($extension == 'gif') {
 				imagegif($this->image, $file);
 			} elseif ($extension == 'webp') {
-				imagewebp($this->image, $file, $quality);
+				imagewebp($this->image, $file);
 			}
              
 			imagedestroy($this->image);
